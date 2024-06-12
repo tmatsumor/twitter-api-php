559c3b1c344bf70a8ae2e8d12561b603e2c8cff6
T. Matsumori <129941863+tmatsumor@users.noreply.github.com>
2024-06-12 23:30:34 +0900

    upd: Changed trigger condition

--- twitter_api.php
+++ twitter_api.php
@@ -12,7 +12,7 @@ class TwitterAPI extends \tmatsumor\http_requests_php\HttpRequests
 
     public function __construct($userpwd) {
         $this->token = json_decode(file_get_contents(self::TWITTER_TOKEN), true);
-        if(time() % 2 === 0){                  // refresh current token if timestamp is even number
+        if(rand(0, 9) % 4 === 0){                  // refresh current token
             $p = 'grant_type=refresh_token'
                . '&refresh_token='.$this->token['refresh_token'];
             $this->token = json_decode($this->post(self::TWITTER_URL.'oauth2/token',
